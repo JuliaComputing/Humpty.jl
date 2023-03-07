ee76cdaed670a274471ea3da2bcb3a72e3824b7a
Frames White <frames.white@invenialabs.co.uk>
2023-03-07 10:21:15 +0800

    Add Single Output test and confirm dead code elimiantion working

--- src/execution.jl
+++ src/execution.jl
@@ -4,12 +4,12 @@ end
 
 function batched_frule((ẋs)::Batch{N}, primal_args...) where N
     ẋ1 = first(ẋs)
-    y, ẏ1 = frule(ẋ1, primal_args...)
+    y, ẏ1 = @inline frule(ẋ1, primal_args...)
     T = typeof(ẏ1)
     ẏs = Vector{T}(undef, N)
     ẏs[1] = ẏ1
     for ii in 2:N
-        _, ẏs[ii] = frule(ẋs[ii], primal_args...)
+        _, ẏs[ii] = @inline frule(ẋs[ii], primal_args...)
     end
 
     return y, Batch{N}(ẏs)
--- test/execution.jl
+++ test/execution.jl
@@ -1,6 +1,6 @@
 
 
-@testset "batched_frule: sin" begin
+@testset "batched_frule: SISO" begin
     @assert frule((NoTangent(), 10.0), sin, 3.0) == (sin(3.0), 10cos(3.0))
     @assert frule((NoTangent(), 100.0), sin, 3.0) == (sin(3.0), 100cos(3.0))
 
@@ -17,6 +17,9 @@
 end
 
 @testset "batched_frule: MIMO" begin
+    # This does not optimize out due to https://github.com/JuliaLang/julia/issues/48808
+    # so the primal gets called once for each partial in the batch
+
     pmimo((x,y)) = [100x+10y, 1000y]
 
     ChainRulesCore.frule((_, (ẋ, ẏ)), ::typeof(pmimo), xy) = pmimo(xy), [100ẋ+10ẏ, 1000ẏ]
@@ -37,4 +40,22 @@ end
     @test convert(Matrix{Float64}, deriv_batch) == [100 10; 0 1000]  # Checked with ForwardDiff.jacobian(pmimo, [2, 3])
 end
 
+@testset "batched_frule: MISO" begin
+    # This optimizes out so that it only called the primal once
+    pmiso((x,y)) = 1000y
+
+    ChainRulesCore.frule((_, (ẋ, ẏ)), ::typeof(pmiso), xy) = pmiso(xy), 1000ẏ
+    @assert frule((NoTangent(), [1.0, 0.0]), pmiso, [2, 3]) == (3000, 0)
+    @assert frule((NoTangent(), [0.0, 1.0]), pmiso, [2, 3]) == (3000, 1000)
+
+
+    @test batched_frule(
+        Batch([
+            Tangent{Tuple{typeof(pmiso), Vector{Float64}}}(NoTangent(), [1.0, 0.0]),
+            Tangent{Tuple{typeof(pmiso), Vector{Float64}}}(NoTangent(), [0.0, 2.0]),
+            Tangent{Tuple{typeof(pmiso), Vector{Float64}}}(NoTangent(), [3.0, 5.0]),
+        ]),
+        pmiso, [2, 3]
+    ) == (3000, Batch([0.0, 2000.0, 5000.0]))
+end 
 
